d1656052106e7b75d06f255c5a59d4a57b77abdb
rotaru-madalina <r.madalina0@gmail.com>
2024-06-01 13:36:16 +0300

    added animations renderer

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    SpriteAnimator animator;
+    public float speed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<SpriteAnimator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Animate();
+    }
+
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        rigidbody.velocity = (new Vector2(horizontalInput, verticalInput).normalized) * speed;
+    }
+
+    private void Animate()
+    {
+        var currentSpeed = rigidbody.velocity.normalized;
+
+        if (currentSpeed == Vector2.left)
+        {
+            animator.PlayAnimation("WalkLeft");
+        }
+        else if (currentSpeed == Vector2.right)
+        {
+            animator.PlayAnimation("WalkRight");
+        }
+        else if (currentSpeed == Vector2.down)
+        {
+            animator.PlayAnimation("WalkBackwards");
+        }
+        else if (currentSpeed == Vector2.up)
+        {
+            animator.PlayAnimation("WalkForward");
+        }
+        else if (currentSpeed == Vector2.zero)
+        {
+            animator.PlayAnimation("Idle");
+        }
+    }
+}
---
+++ Assets/SpriteAnimator.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour
+{
+    [System.Serializable] public struct SpriteAnimation
+    {
+        public string name;
+        public List<Sprite> sprites;
+        public float speed;
+    }
+    public List<SpriteAnimation> animations;
+    private SpriteRenderer spriteRenderer;
+
+    public void PlayAnimation(string animationName)
+    {
+        foreach (var animation in animations)
+        {
+            if(animation.name == animationName)
+            {
+                StopAllCoroutines();
+                StartCoroutine(AnimationCoroutine(animation));
+            }
+            
+        }
+    }
+    public IEnumerator AnimationCoroutine(SpriteAnimation animation)
+    {
+        int currentSpriteIdx = 0;
+        while(true)
+        {
+            spriteRenderer.sprite = animation.sprites[currentSpriteIdx];
+            currentSpriteIdx++;
+
+            if(currentSpriteIdx == animation.sprites.Count)
+                currentSpriteIdx = 0;
+
+            yield return new WaitForSeconds(animation.speed);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
